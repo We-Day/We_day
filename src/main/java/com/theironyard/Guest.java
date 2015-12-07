eb323ca24792c9701f3c814a5f23a1b81995e75d
macbookair <nj.martin123@gmail.com>
2015-12-07 13:52:00 -0500

    Initial Import

---
+++ src/main/java/com/theironyard/Guest.java
@@ -0,0 +1,15 @@
+package com.theironyard;
+
+/**
+ * Created by macbookair on 12/7/15.
+ */
+public class Guest {
+    String firstName;
+    String lastName;
+    String email;
+    int phone;
+    String zip;
+    String password;
+    boolean plusOne;
+    boolean isAdmin;
+}
