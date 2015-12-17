5d70a282427d4cfdb4301fdc5e6fab02399de700
Charles Lane <cglane@Charless-MacBook-Pro.local>
2015-12-17 09:28:09 -0500

    hello

--- public/app/main/login.controller.js
+++ public/app/main/login.controller.js
@@ -3,6 +3,20 @@
 angular
   .module('main')
   .controller('LoginController',function($scope,LoginService){
-
+    var users = [];
+    LoginService.getUsers().success(function(res){
+      var users = res;
+    })
+    $scope.loginUser = function(email,password){
+      _.each(users,function(el){
+        if(el.email === email){
+          if(el.password === password){
+            $location.path('/landingPage/'+el.id);
+            return true;
+          }
+        }return false;
+        }
+      })
+    }
   });
 })();
--- public/app/main/login.services.js
+++ public/app/main/login.services.js
@@ -3,9 +3,13 @@
   angular
     .module('main')
     .factory('LoginService',function($http){
-
+      var urlUsers = '/create-users';
+      var getUsers = function(){
+        return $http.get(urlUsers);
+      }
 
     return{
+      getUsers:getUsers,
     };
   });
 })();
