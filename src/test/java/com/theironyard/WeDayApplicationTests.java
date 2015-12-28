65cc5137c7a9902e0b5ae6691bbb9902e3450626
macbookair <nj.martin123@gmail.com>
2015-12-28 12:52:32 -0500

    Added a rough "/join-wedding" route

--- src/test/java/com/theironyard/WeDayApplicationTests.java
+++ src/test/java/com/theironyard/WeDayApplicationTests.java
@@ -1,87 +1,88 @@
-//
-//    package com.theironyard;
-//    import com.fasterxml.jackson.core.JsonProcessingException;
-//    import com.theironyard.Entities.Post;
-//    import com.theironyard.Entities.User;
-//    import com.theironyard.Entities.Wedding;
-//    import com.theironyard.Services.PostRepository;
-//    import com.theironyard.Services.UserRepository;
-//    import com.theironyard.Services.WeddingRepository;
-//    import com.theironyard.Utilities.PasswordHash;
-//    import org.junit.Before;
-//    import org.junit.Test;
-//    import org.junit.runner.RunWith;
-//    import org.springframework.beans.factory.annotation.Autowired;
-//    import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-//    import org.springframework.context.annotation.Bean;
-//    import org.springframework.mail.javamail.JavaMailSenderImpl;
-//    import org.springframework.mail.javamail.MimeMessageHelper;
-//    import org.springframework.test.context.web.WebAppConfiguration;
-//    import org.springframework.boot.test.SpringApplicationConfiguration;
-//    import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-//    import org.springframework.test.web.servlet.MockMvc;
-//    import com.fasterxml.jackson.databind.ObjectMapper;
-//    import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-//    import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-//    import org.springframework.web.context.WebApplicationContext;
-//
-//    import javax.mail.MessagingException;
-//    import javax.mail.internet.MimeMessage;
-//    import javax.servlet.http.HttpSession;
-//
-//    import static org.junit.Assert.assertTrue;
-//
-//    @RunWith(SpringJUnit4ClassRunner.class)
-//    @SpringApplicationConfiguration(classes = WeDayApplication.class)
-//    @WebAppConfiguration
-//    public class WeDayApplicationTests {
-//
-//        @Autowired
-//        WeddingRepository weddings;
-//
-//        @Autowired
-//        UserRepository users;
-//
-//        @Autowired
-//        PostRepository posts;
-//
-//        @Autowired
-//        WebApplicationContext wap;
-//
-//        MockMvc mockMvc;
-//
-//        @Before
-//        public void before() {
-//            //weddings.deleteAll();
-//            users.deleteAll();
-//            posts.deleteAll();
-//
-//            mockMvc = MockMvcBuilders.webAppContextSetup(wap).build();
-//        }
-//
-//        @Test
-//        public void testLogin() throws Exception {
-//
-//            users.deleteAll();
-//
-//            String password = "password";
-//
-//            User user = new User();
-//            user.email = "nathan@gmail.com";
-//            user.username = "Nathan";
-//            user.password = PasswordHash.createHash(password);
-//            user.phone = "123-4567";
-//            users.save(user);
-//
-//            mockMvc.perform(
-//                    MockMvcRequestBuilders.post("/login")
-//                            .param("password", password)
-//                            .param("email", user.email)
-//            );
-//
-//            assertTrue(users.count() == 1 && PasswordHash.validatePassword(password, user.password));
-//        }
-//
+
+    package com.theironyard;
+    import com.fasterxml.jackson.core.JsonProcessingException;
+    import com.theironyard.Entities.Post;
+    import com.theironyard.Entities.User;
+    import com.theironyard.Entities.Wedding;
+    import com.theironyard.Services.PostRepository;
+    import com.theironyard.Services.UserRepository;
+    import com.theironyard.Services.WeddingRepository;
+    import com.theironyard.Utilities.PasswordHash;
+    import org.junit.Before;
+    import org.junit.Test;
+    import org.junit.runner.RunWith;
+    import org.springframework.beans.factory.annotation.Autowired;
+    import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+    import org.springframework.context.annotation.Bean;
+    import org.springframework.mail.javamail.JavaMailSenderImpl;
+    import org.springframework.mail.javamail.MimeMessageHelper;
+    import org.springframework.test.context.web.WebAppConfiguration;
+    import org.springframework.boot.test.SpringApplicationConfiguration;
+    import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+    import org.springframework.test.web.servlet.MockMvc;
+    import com.fasterxml.jackson.databind.ObjectMapper;
+    import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+    import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+    import org.springframework.web.context.WebApplicationContext;
+
+    import javax.mail.MessagingException;
+    import javax.mail.internet.MimeMessage;
+    import javax.servlet.http.HttpSession;
+
+    import static org.junit.Assert.assertTrue;
+
+    @RunWith(SpringJUnit4ClassRunner.class)
+    @SpringApplicationConfiguration(classes = WeDayApplication.class)
+    @WebAppConfiguration
+    public class WeDayApplicationTests {
+
+        @Autowired
+        WeddingRepository weddings;
+
+        @Autowired
+        UserRepository users;
+
+        @Autowired
+        PostRepository posts;
+
+        @Autowired
+        WebApplicationContext wap;
+
+        MockMvc mockMvc;
+
+        @Before
+        public void before() {
+            //weddings.deleteAll();
+            users.deleteAll();
+            posts.deleteAll();
+
+            mockMvc = MockMvcBuilders.webAppContextSetup(wap).build();
+        }
+
+        @Test
+        public void testLogin() throws Exception {
+
+            users.deleteAll();
+
+            String password = "password";
+
+            User user = new User();
+            user.email = "nathan@gmail.com";
+            user.username = "Nathan";
+            user.password = PasswordHash.createHash(password);
+            user.phone = "123-4567";
+            users.save(user);
+
+            mockMvc.perform(
+                    MockMvcRequestBuilders.post("/login")
+                            .param("password", password)
+                            .param("email", user.email)
+            );
+
+            assertTrue(users.count() == 1 && PasswordHash.validatePassword(password, user.password));
+        }
+    }
+
 //        @Test
 //        public void createUser() throws Exception {
 //            users.deleteAll();
