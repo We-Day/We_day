b302003af2e5a889609f7e54c0db1d0b5deed658
macbookair <nj.martin123@gmail.com>
2016-01-04 13:34:50 -0500

    fixed add event

--- src/main/java/com/theironyard/Entities/CalendarEvent.java
+++ src/main/java/com/theironyard/Entities/CalendarEvent.java
@@ -1,93 +1,122 @@
-    package com.theironyard.Entities;
+package com.theironyard.Entities;
 
-    import javax.persistence.*;
-    import java.time.LocalDate;
-    import java.time.LocalDateTime;
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
-    /**
-     * Created by macbookair on 12/16/15.
-     */
+/**
+ * Created by macbookair on 12/16/15.
+ */
 
-    @Entity
-    @Table (name = "events")
-    public class CalendarEvent {
-        @Id
-        @GeneratedValue
-        public int _id;
+@Entity
+@Table (name = "events")
+public class CalendarEvent {
+    @Id
+    @GeneratedValue
+    public int _id;
 
-        @Column(nullable = false)
-        public String start;
+    @Column(nullable = false)
+    public String start;
 
-        @Column(nullable = false)
-        public String end;
+    @Column(nullable = false)
+    public String end;
 
-        @Column(nullable = false)
-        public String title;
+    @Column(nullable = false)
+    public String title;
 
-        @ManyToOne
-        public Wedding wedding;
+    @ManyToOne
+    public Wedding wedding;
 
-        public CalendarEvent(){
+    public Boolean emailBool;
+    public String emailTime;
 
-        }
+    public Boolean textBool;
+    public String textTime;
 
+    public Boolean notificationBool;
+    public String notificationTime;
 
-        public Boolean emailBool;
-        public String emailTime;
-
-        public Boolean textBool;
-        public String textTime;
-
-        public Boolean notificationBool;
-        public Boolean notificationTime;
-
-        public Boolean getNotificationTime() {
-            return notificationTime;
-        }
-
-        public void setNotificationTime(Boolean notificationTime) {
-            this.notificationTime = notificationTime;
-        }
-
-        public Boolean getNotificationBool() {
-            return notificationBool;
-        }
-
-        public void setNotificationBool(Boolean notificationBool) {
-            this.notificationBool = notificationBool;
-        }
-
-        public String getTextTime() {
-            return textTime;
-        }
-
-        public void setTextTime(String textTime) {
-            this.textTime = textTime;
-        }
-
-        public Boolean getTextBool() {
-            return textBool;
-        }
-
-        public void setTextBool(Boolean textBool) {
-            this.textBool = textBool;
-        }
-
-        public String getEmailTime() {
-            return emailTime;
-        }
-
-        public void setEmailTime(String emailTime) {
-            this.emailTime = emailTime;
-        }
-
-        public Boolean getEmailBool() {
-            return emailBool;
-        }
-
-        public void setEmailBool(Boolean emailBool) {
-            this.emailBool = emailBool;
-        }
-
-
+    public CalendarEvent(){
     }
+
+
+    public String getStart() {
+        return start;
+    }
+
+    public void setStart(String start) {
+        this.start = start;
+    }
+
+    public String getEnd() {
+        return end;
+    }
+
+    public void setEnd(String end) {
+        this.end = end;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Wedding getWedding() {
+        return wedding;
+    }
+
+    public void setWedding(Wedding wedding) {
+        this.wedding = wedding;
+    }
+
+    public Boolean getEmailBool() {
+        return emailBool;
+    }
+
+    public void setEmailBool(Boolean emailBool) {
+        this.emailBool = emailBool;
+    }
+
+    public String getEmailTime() {
+        return emailTime;
+    }
+
+    public void setEmailTime(String emailTime) {
+        this.emailTime = emailTime;
+    }
+
+    public Boolean getTextBool() {
+        return textBool;
+    }
+
+    public void setTextBool(Boolean textBool) {
+        this.textBool = textBool;
+    }
+
+    public String getTextTime() {
+        return textTime;
+    }
+
+    public void setTextTime(String textTime) {
+        this.textTime = textTime;
+    }
+
+    public Boolean getNotificationBool() {
+        return notificationBool;
+    }
+
+    public void setNotificationBool(Boolean notificationBool) {
+        this.notificationBool = notificationBool;
+    }
+
+    public String getNotificationTime() {
+        return notificationTime;
+    }
+
+    public void setNotificationTime(String notificationTime) {
+        this.notificationTime = notificationTime;
+    }
+}
